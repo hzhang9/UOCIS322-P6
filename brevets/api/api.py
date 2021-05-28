41a01f525b8ccfe04ef714bac3332a837f17bd4c
Haoran Zhang <hzhang9@testium.cs.uoregon.edu>
2021-05-27 23:32:08 -0700

    Api can shows default and json(same)

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -1,44 +1,88 @@
-# Streaming Service
-
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource, Api
+import pymongo
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 api = Api(app)
+client= MongoClient('mongodb://'+os.environ['MONGODB_HOSTNAME'],27017)
+db=client.tododb
 
+class listAJ(Resource):
+    def get(self):
+        top=request.args.get("top")
+        result=[]
+        items=list(db.tododb.find())
+        counter=1
+        bre_order={}
+        if top==None:
+            for item in items:
+                result.append({'ACP Brevet Times Instance {}'.format(counter):{'miles':item['miles'],'km':item['km'],'location':item['location'],'open':item['open'],'close':item['close']}})
+                counter+=1
+            return {'ACP Brevet Times Display':result}
+        else:
+            for i in range(int(top)):
+                result.append({'ACP Brevet Times Instance {}'.format(counter):{'miles':items[i]['miles'],'km':items[i]['km'],'location':items[i]['location'],'open':items[i]['open'],'close':items[i]['close']}})
+                counter+=1
+            return {'ACP Brevet Times Display':result}
 
-class listAllJson(Resource):
+class listOJ(Resource):
+    def get(self):
+        top=request.args.get("top")
+        result=[]
+        items=list(db.tododb.find())
+        counter=1
+        bre_order={}
+        if top==None:
+            for item in items:
+                result.append({'ACP Brevet Open Times Instance {}'.format(counter) :{'miles':item['miles'],'km':item['km'],'location':item['location'],'open':item['open']}})
+                counter+=1
+            return {'ACP Brevet Open Times Display':result}
+        else:
+            for i in range(int(top)):
+                result.append({'ACP Brevet Open Times Instance {}'.format(counter):{'miles':items[i]['miles'],'km':items[i]['km'],'location':items[i]['location'],'open':items[i]['open']}})
+                counter+=1
+            return {'ACP Brevet Open Times Display':result}
+
+class listCJ(Resource):
+    def get(self):
+        top=request.args.get("top")
+        result=[]
+        items=list(db.tododb.find())
+        counter=1
+        bre_order={}
+        if top==None:
+            for item in items:
+                result.append({'ACP Brevet Close  Times Instance {}'.format(counter) :{'miles':item['miles'],'km':item['km'],'location':item['location'],'close':item['close']}})
+                counter+=1
+            return {'ACP Brevet Close Times Display':result}
+        else:
+            for i in range(int(top)):
+                result.append({'ACP Brevet Close Times Instance {}'.format(counter) :{'miles':items[i]['miles'],'km':items[i]['km'],'location':items[i]['location'],'close':items[i]['close']}})
+                counter+=1
+            return {'ACP Brevet Close Times Display':result}
+
+class listAC(Resource):
     def get(self):
         return None
 
-class listOpenJson(Resource):
+class listOC(Resource):
     def get(self):
         return None
 
-class listCloseJson(Resource):
-    def get(self):
-        return None
-
-class listAllCsv(Resource):
-    def get(self):
-        return None
-
-class listOpenCsv(Resource):
-    def get(self):
-        return None
-
-class listCloseCsv(Resource):
+class listCC(Resource):
     def get(self):
         return None
 
 # Create routes
 # Another way, without decorators
-api.add_resource(listAllJson, '/listAll', '/listAll/json')
-api.add_resource(listOpenJson, '/listOpenOnly', '/listOpenOnly/json')
-api.add_resource(listCloseJson, '/listCloseOnly', '/listCloseOnly/json')
-api.add_resource(listAllCsv, '/listAll/csv')
-api.add_resource(listOpenCsv, '/listOpenOnly/csv')
-api.add_resource(listCloseCsv, '/listCloseOnly/csv')
+api.add_resource(listAJ, '/listAll', '/listAll/json')
+api.add_resource(listOJ, '/listOpenOnly', '/listOpenOnly/json')
+api.add_resource(listCJ, '/listCloseOnly', '/listCloseOnly/json')
+api.add_resource(listAC, '/listAll/csv')
+api.add_resource(listOC, '/listOpenOnly/csv')
+api.add_resource(listCC, '/listCloseOnly/csv')
 
 # Run the application
 if __name__ == '__main__':
